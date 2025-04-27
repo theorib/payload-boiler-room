169cd58c07bddb02f9e40f672d06ca1ace14c540
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-04-27 23:04:21 +0100

    ♻️ refactor: move payload related files to a dedicated folder
    - 【Refactor】: Move all payload-related files into a `payload` folder for better organization
    - 【File Changes】:
      - Move `src/collections/Media.ts` to `src/payload/collections/Media.ts`
      - Move `src/collections/Users.ts` to `src/payload/collections/Users.ts`
      - Move `src/payload-types.ts` to `src/payload/payload-types.ts`
      - Move `src/payload.config.ts` to `src/payload/payload.config.ts`
    - 【Update Imports】: Update all import statements to reflect the new file locations

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -4,8 +4,10 @@ import { getPayload } from 'payload'
 import React from 'react'
 import { fileURLToPath } from 'url'
 
-import config from '@/payload.config'
+import config from '@/payload/payload.config'
 import './styles.css'
+import Link from 'next/link'
+import { buttonVariants } from '@/components/ui/button'
 
 export default async function HomePage() {
   const headers = await getHeaders()
@@ -16,41 +18,50 @@ export default async function HomePage() {
   const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
 
   return (
-    <div className="home">
-      <div className="content">
-        <picture>
+    <div className="m-auto flex h-full max-w-lg flex-col items-center justify-between p-11">
+      <div className="flex grow flex-col items-center justify-between">
+        <picture className="">
           <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
           <Image
+            className="block h-auto max-w-full invert"
             alt="Payload Logo"
             height={65}
             src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
             width={65}
           />
         </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
+        {!user && (
+          <h1 className="my-10 text-center text-6xl leading-16 font-bold">
+            Welcome to your new project.
+          </h1>
+        )}
+        {user && (
+          <h1 className="my-10 text-center text-6xl leading-16 font-bold">
+            Welcome back, {user.email}
+          </h1>
+        )}
+        <div className="flex items-center gap-3">
+          <Link
+            className={buttonVariants({ variant: 'default' })}
             href={payloadConfig.routes.admin}
             rel="noopener noreferrer"
             target="_blank"
           >
             Go to admin panel
-          </a>
-          <a
-            className="docs"
+          </Link>
+          <Link
+            className={buttonVariants({ variant: 'outline' })}
             href="https://payloadcms.com/docs"
             rel="noopener noreferrer"
             target="_blank"
           >
             Documentation
-          </a>
+          </Link>
         </div>
       </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
+      <div className="flex items-center gap-2">
+        <p className="m-6">Update this page by editing</p>
+        <a className="bg-neutral-600 px-2 py-1 text-neutral-300" href={fileURL}>
           <code>app/(frontend)/page.tsx</code>
         </a>
       </div>
--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -6,9 +6,10 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { Users } from '@/payload/collections/Users'
+import { Media } from '@/payload/collections/Media'
 
-import { Users } from './collections/Users'
-import { Media } from './collections/Media'
+
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
